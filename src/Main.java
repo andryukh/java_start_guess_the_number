28be92af60a773da5adb9bd51441b4e41e3cf034
Yukha <andr2350@gmail.com>
2023-02-24 12:19:12 +0300

    Цикл while перенесён внутр нового метода playLevel

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Ваша задача - угадать число!!1");
         System.out.println("Введите верхнюю границу диапазона: ");
         int range = scanner.nextInt();
         int number = (int)(Math.random() * range);
+        playLevel(range, number);
+        scanner.close();
+    }
+    private static void playLevel(int range, int number) {
         while(true) {
             System.out.println("Угадайте число от 0 до " + range);
             int input_number = scanner.nextInt();
@@ -19,6 +24,5 @@ public class Main {
                 System.out.println("Загаданное число больше!!1");
             }
         }
-        scanner.close();
     }
 }
